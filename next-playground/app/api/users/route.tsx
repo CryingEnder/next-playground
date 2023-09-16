f8c6478b4d656c9406bf37e0ca0e755c573ed132
Cristian Botez <cristi@iotnetpro.com>
2023-09-16 14:03:16 +0300

    Add DB

--- next-playground/app/api/users/route.tsx
+++ next-playground/app/api/users/route.tsx
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import { PrismaClient } from "@prisma/client";
 
-export function GET(request: NextRequest) {
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
   // you need to have "request", or it'll cache data
-  return NextResponse.json([
-    { id: 1, name: "Mosh" },
-    { id: 2, name: "Michael" },
-  ]);
+
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
@@ -17,5 +19,17 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const user = await prisma.user.findUnique({ where: { email: body.email } });
+  if (user) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
