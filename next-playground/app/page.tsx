9fb54f7f230020dd2a5d87b8506fdd610842a07e
Cristian Botez <cristi@iotnetpro.com>
2023-09-18 14:31:24 +0300

    Add google auth part 1

--- next-playground/app/page.tsx
+++ next-playground/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1 className="text-black">Yo</h1>
+      <h1 className="text-black">
+        Yo {session && <span>{session.user!.name}</span>}
+      </h1>
     </main>
   );
 }
